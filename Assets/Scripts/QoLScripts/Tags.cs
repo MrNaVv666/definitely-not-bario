67b027822e880977d0d63c14cb6df694f1e6058f
Hubert Nowacki <H.Nowacki@protonmail.com>
2024-06-18 08:36:31 +0200

    Update Tags.cs

--- Assets/Scripts/QoLScripts/Tags.cs
+++ Assets/Scripts/QoLScripts/Tags.cs
@@ -5,5 +5,7 @@ using UnityEngine;
 public class Tags : MonoBehaviour
 {
     public static string PLAYER_TAG = "Player";
+    public static string SNAIL_TAG = "Snail";
+    public static string BEETLE_TAG = "Beetle";
     public static string BULLET_TAG = "Bullet";
 }
