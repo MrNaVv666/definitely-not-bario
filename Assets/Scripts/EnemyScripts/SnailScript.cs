90d86f423cf5e3ae5f7231e47d474d1f2c6c88d9
Hubert Nowacki <H.Nowacki@protonmail.com>
2024-06-22 17:26:39 +0200

    Update SnailScript.cs

--- Assets/Scripts/EnemyScripts/SnailScript.cs
+++ Assets/Scripts/EnemyScripts/SnailScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SnailScript : MonoBehaviour
 {
-    public float speed = 2f;
+    public float speed = 5f;
     private bool moveLeft, canMove, stunned;
     private Rigidbody2D snailBody;
     private Animator animator;
