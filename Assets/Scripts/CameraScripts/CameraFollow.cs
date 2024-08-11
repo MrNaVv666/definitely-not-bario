f1f89bc6dd1e0889a8a7cd2827e9ed22c4b69b23
Hubert Nowacki <H.Nowacki@protonmail.com>
2024-08-11 14:59:22 +0200

    CameraFollow Implementation

--- Assets/Scripts/CameraScripts/CameraFollow.cs
+++ Assets/Scripts/CameraScripts/CameraFollow.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float resetSpeed = 0.5f;
+    public float cameraSpeed = 0.3f;
+
+    public Bounds cameraBounds;
+
+    private Transform target;
+
+    private float offsetZ;
+    private Vector3 lastTargetPosition;
+    private Vector3 currentVelocity;
+
+    private bool followsPlayer;
+
+    private void Awake()
+    {
+        BoxCollider2D myCol = GetComponent<BoxCollider2D>();
+        myCol.size = new Vector2(Camera.main.aspect * 2f * Camera.main.orthographicSize, 15f);
+        cameraBounds = myCol.bounds;
+    }
     void Start()
     {
-        
+        target = GameObject.FindGameObjectWithTag(Tags.PLAYER_TAG).transform;
+        lastTargetPosition = target.position;
+        offsetZ = (transform.position - target.position).z;
+        followsPlayer = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        if (followsPlayer)
+        {
+            Vector3 aheadTargetPos = target.position + Vector3.forward * offsetZ;
+
+            if (aheadTargetPos.x != transform.position.x)
+            {
+                Vector3 newCameraPosition = Vector3.SmoothDamp(transform.position, aheadTargetPos, ref currentVelocity, cameraSpeed);
+
+                transform.position = new Vector3(newCameraPosition.x, newCameraPosition.y + 0.1f, newCameraPosition.z);
+
+                lastTargetPosition = target.position;
+            }
+        }
     }
 }
